d825c8be2a05367b901e91271d88aa8bb276e86b
dslu7733 <dslu7733@163.com>
2019-10-15 09:12:08 +0800

    save the pratice range

---
+++ server/db/static/praticeRange.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/oralCal', {useFindAndModify:false,useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+
+let praRangeSchema = new mongoose.Schema({
+  grade: {
+    type: Array,
+    required: true
+  },
+
+  praRange: {
+    type: Array,
+    require: true
+  }
+})
+
+const PraRange = mongoose.model('praticeRange', praRangeSchema)
+
+var gradeOneFirst = new PraRange({
+  grade: ['gradeOne','firstSemester'],
+
+  praRange: ['十几减9',
+             '十几减8，7，6',
+             '十几减5,4,3,2',
+             '整十数加，整十数减'
+  ]
+}).save((err,res)=>{
+    if(err){
+      console.log('update praticeRange err: ' + err)
+    }else{
+      console.log('update pratice range: ' + res)
+    }
+})
